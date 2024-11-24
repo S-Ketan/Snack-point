8cfe3760dd053aa6c85d4ccb0b763cbb2105b4cd
Ketan Sharma <ketan10923@gmail.com>
2024-11-24 09:40:26 +0100

    Page redesigned

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,29 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{html,js}"],
+  content: [
+    "./index.html", // Include the root-level index.html
+    "./src/**/*.{html,js,ts,jsx,tsx}", // Include all source files where Tailwind classes are used
+  ],
   theme: {
-    extend: {},
+    extend: {  scrollbar: {
+      'hide': {
+        'overflow': 'hidden',
+      },
+    },},
   },
-  plugins: [],
+  plugins: [  function({ addUtilities }) {
+    addUtilities({
+      '.scrollbar-hide': {
+        'overflow': 'hidden',
+      },
+      '.scrollbar-hide::-webkit-scrollbar': {
+        display: 'none',
+      },
+      '.scrollbar-hide': {
+        '-ms-overflow-style': 'none',
+        'scrollbar-width': 'none',
+      },
+    }, ['responsive', 'hover']);
+  },],
 }
 
